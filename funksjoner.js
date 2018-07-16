f907bedf3b47f35d8c11b2d51a4d93428becf9f1
May Tove Alseth <alma1409@akershus-fk.no>
2018-07-16 17:42:27 +0200

    Lag til lagtabell funksjon

--- funksjoner.js
+++ funksjoner.js
@@ -19,3 +19,30 @@ function sammenlignLand(a,b){
     return 0;
   }
 }
+function lagTabell(overskrifter, innhold) {
+  // vil legge til en tabell i .innpakning. Overskrifter(array)-header i tabellen.
+  // innhold(array med objekter). Selve innholdet i tabellen.
+
+  //Lager tabell og legger inn overskrifter
+  var innpakningEl = document.querySelector(".innpakning");
+  var tabellEl = document.createElement("table");
+  var tbodyEl = document.createElement("tbody");
+  var overskrift = "<tr>";
+  for (var i = 0; i < overskrifter.length; i++) {
+    overskrift += "<th>" + overskrifter[i] + "</th>";
+  }
+  overskrift += "</tr>";
+  tbodyEl.innerHTML+=overskrift;
+  //Legger inn innhold fra objektet som skal i tabellen
+  for (var i = 0; i < innhold.length; i++) {
+    var rad = "<tr>";
+    for (var egenskap in innhold[i]) {
+      rad +="<td>"+innhold[i][egenskap]+"</td>";
+    }
+    rad +="</tr>";
+    tbodyEl.innerHTML+=rad;
+    tabellEl.appendChild(tbodyEl);
+    innpakningEl.appendChild(tabellEl);
+  }
+
+}
