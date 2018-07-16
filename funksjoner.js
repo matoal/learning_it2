4b230614696342f4249c77093eda4e7db85c12de
May Tove Alseth <alma1409@akershus-fk.no>
2018-07-16 16:00:28 +0200

    kap 12

--- funksjoner.js
+++ funksjoner.js
@@ -1,6 +1,21 @@
 function sammenlignTall(a,b){
-  return a-b
+  if(a>b){
+    return 1;
+  } else if (a<b) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
 function sammenlignArrayer(a,b) {
   return a[1]-b[1];
+}
+function sammenlignLand(a,b){
+  if(a.land>b.land){
+    return 1;
+  } else if (a.land<b.land) {
+    return -1;
+  } else {
+    return 0;
+  }
 }
